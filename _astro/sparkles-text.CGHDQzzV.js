1b3206b0bfc23cfcb6e4100b60a64a70f4e2d022
Arnav Dewan <57629464+arnxv0@users.noreply.github.com>
2025-06-21 02:01:41 +0530

    Change folder structure

--- _astro/sparkles-text.CGHDQzzV.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./sparkles-text.DZacxWiV.js";import"./jsx-runtime.D_zvdyIk.js";import"./index.ai7qpRr1.js";import"./utils.CrUd9PdB.js";export{e as SparklesText};
--- dist/_astro/index.BpqtGe_y.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index.BMjRLAIj.js";var a=r();export{a as r};
